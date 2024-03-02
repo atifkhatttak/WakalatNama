1123208e76a24b619204f8535db070fe2319c1de
Atif Khattak <atifkhattak304@gmail.com>
2024-03-02 18:21:51 +0500

    Mark Read Notification API

--- WKLNAMA/Controllers/NotificationController.cs
+++ WKLNAMA/Controllers/NotificationController.cs
@@ -2,6 +2,8 @@
 using Data.DomainModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using WKLNAMA.AppHub;
 
 namespace WKLNAMA.Controllers
 {
@@ -10,10 +12,28 @@ namespace WKLNAMA.Controllers
     public class NotificationController : BaseController<Notification>
     {
         private readonly INotificationRepository _notificationService;
+        private readonly ChatHub _chatHub;
 
-        public NotificationController(INotificationRepository notificationService, IHttpContextAccessor httpContextAccessor, ILogger<NotificationController> logger): base(notificationService, httpContextAccessor, logger)
+        public NotificationController(INotificationRepository notificationService, IHttpContextAccessor httpContextAccessor, ILogger<NotificationController> logger, ChatHub chatHub): base(notificationService, httpContextAccessor, logger)
         {
             _notificationService = notificationService;
+            _chatHub = chatHub;
+        }
+
+        [HttpPost("MarkRead")]
+        public async Task<ActionResult> MarkRead(List<long> ids)
+        {
+            return await APIResponse( async () => {
+
+                await _notificationService.MarkIsRead(ids);
+
+                apiResponse.Message = HttpStatusCode.OK.ToString();
+                apiResponse.HttpStatusCode = HttpStatusCode.OK;
+                apiResponse.Success = true;
+                apiResponse.Data = "Messages have been marked as Read";
+
+                return Ok(apiResponse);
+            });
         }
     }
 }
