36089e27317fc2c4ec80c81a61ec6d0e293fc1e8
Atif Khattak <atifkhattak304@gmail.com>
2024-03-02 11:39:47 +0500

    Added Notification Type

---
+++ Business/Enums/NotificationType.cs
@@ -0,0 +1,20 @@
+﻿using Business.Helpers.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Enums
+{
+    public enum NotificationType :int
+    {
+        [Message("Hi, {FullName} Welcome to WakalatNaama")]
+        [Description("Greeting")]
+        Greeting =1,
+        [Message("ForgotPassword")]
+        [Description("ForgotPassword")]
+        ForgotPassword,
+    }
+}
