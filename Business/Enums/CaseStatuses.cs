0db0a3165480c1a67abbd819906c368b4b9bb17a
Atif Khattak <atifkhattak304@gmail.com>
2024-03-16 14:48:39 +0500

    Case Status Enum Added

---
+++ Business/Enums/CaseStatuses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Enums
+{
+  public enum  CaseStatuses
+    {
+        New=1,
+        AdminAccepted=2,
+        AdminRejected=3,
+        LawyerAccepted=4,
+        LawyerRejected=5,
+    }
+}
