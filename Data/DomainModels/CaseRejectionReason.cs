abd741f735c9ff042b43bc5df734b7a2e3a76311
Atif Khattak <atifkhattak304@gmail.com>
2024-03-16 16:57:16 +0500

    Case Rejection Schema Added

---
+++ Data/DomainModels/CaseRejectionReason.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DomainModels
+{
+    public class CaseRejectionReason : BaseModel
+    {
+        public int Id { get; set; }
+        public long CaseId { get; set; }
+        public long RejectById { get; set; }
+        public string Reason { get; set; }
+        public int Status { get; set; }  
+
+    }
+}
